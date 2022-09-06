f92db79aa6debbb1c90db132334b67d45a52c7c9
Nguyễn Văn Bình <binhvnth2011019@fpt.edu.vn>
2022-09-07 06:22:59 +0700

    done form

--- src/app/pages/product-form/product-form.component.ts
+++ src/app/pages/product-form/product-form.component.ts
@@ -5,7 +5,7 @@ import {
   AfterViewInit,
   Renderer2,
 } from '@angular/core';
-import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
@@ -27,14 +27,31 @@ export class ProductFormComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.productForm = this.fb.group({
       info: this.fb.group({
-        id: [''],
-        street: [''],
-        pincode: [''],
+        idProduct: ['', [Validators.required]],
+        nameProduct: ['', [Validators.required]],
+        secondNameProduct: [''],
+        unitProduct: ['', [Validators.required]],
+        typeProduct: ['', [Validators.required]],
+        groupProduct: ['', [Validators.required]],
+        isAutoItemCodeProduct: [false],
+        itemCodeProduct: ['', [Validators.required]],
+        noteProduct: [''],
+        thumbnailProduct: [''],
       }),
       attribute: this.fb.group({
-        city: [''],
-        street: [''],
-        pincode: [''],
+        lengthProduct: ['', [Validators.required]],
+        widthProduct: ['', [Validators.required]],
+        heightProduct: ['', [Validators.required]],
+        weightProduct: ['', [Validators.required]],
+        standardGroupQc: ['', [Validators.required]],
+        typeOfPurchase: ['', [Validators.required]],
+        shortCode: ['', [Validators.required]],
+        isSerial: [false],
+        isInventoryByLocation: [false],
+        isIroningUniform: [false],
+        isStampScale: [false],
+        isUsingItem: [false],
+        dueDateProduct: ['', [Validators.required]],
       }),
     });
   }
@@ -42,8 +59,11 @@ export class ProductFormComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     this.tabLinks = this.el.nativeElement.querySelectorAll('.tab__link');
     this.tabContents = this.el.nativeElement.querySelectorAll('.tabContent');
-    this.el.nativeElement.querySelector('#defaultOpen').click();
+    setTimeout(() => {
+      this.el.nativeElement.querySelector('#defaultOpen').click();
+    });
   }
+
   //**open tab  */
   public openTab(evt: any, tabName: string): void {
     let currentTab = this.el.nativeElement.querySelector(`#${tabName}`);
