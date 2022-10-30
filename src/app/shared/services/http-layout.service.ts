fc140a787ee457418143107f58f7ab15d2bb6209
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2022-10-30 17:16:52 +0700

    switch layout

--- src/app/shared/services/http-layout.service.ts
+++ src/app/shared/services/http-layout.service.ts
@@ -8,9 +8,8 @@ import { IWjFlexColumnConfig, IWjFlexLayoutConfig } from '../data-type/wijmo-dat
 })
 export class HttpLayoutService {
   public formTabs$: Observable<any> = this._http.get<any>('assets/data/layout-form.data.json');
-  public wijFlexLayout$: Observable<IWjFlexLayoutConfig> = this._http.get<any>('assets/data/wjFlex-layout-config.data.json');
+  public wijFlexLayout$: Observable<IWjFlexLayoutConfig> = this._http.get<any>('assets/data/wjFlex-layout-3-config.data.json');
   public wijFlexColumnConfig$: Observable<IWjFlexColumnConfig> = this._http.get<any>('assets/data/colFlex-product-config.data.json');
-
   constructor(private _http: HttpClient) { };
 
 
