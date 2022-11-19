57b6903a5c599e3adb6a8e164cbfc60ea5f72288
Nguyễn Văn Bình <78024720+Congchuabongbong@users.noreply.github.com>
2022-11-19 09:05:34 +0700

    nothing

--- src/app/pages/demo-collection-view/demo-collection-view.component.ts
+++ src/app/pages/demo-collection-view/demo-collection-view.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
-import { map, Observable, tap, AsyncSubject, switchMap, of, Subject, combineLatest, startWith, takeUntil, interval, takeWhile, filter, merge } from 'rxjs';
+import { map, Observable, tap, switchMap, of, Subject, combineLatest, startWith, takeUntil, interval, takeWhile, filter, merge } from 'rxjs';
 import * as wjcInput from '@grapecity/wijmo.input';
 import { setCss, PropertyGroupDescription, IPredicate, IGetError, IEventHandler, NotifyCollectionChangedEventArgs, EventArgs, CancelEventArgs, ObservableArray, Event, Binding, asFunction, createElement, tryCast, isNullOrWhiteSpace, SortDescription, Globalize, ArrayBase, CollectionView } from '@grapecity/wijmo';
 @Component({
@@ -14,53 +14,13 @@ export class DemoCollectionViewComponent implements OnInit, OnDestroy {
   private addNewSubject = new Subject<any>();
   public addNewAction$ = this.addNewSubject.asObservable();
   private notifierCompleted = new Subject();
-  public selectedAction = this.selectedSubject.asObservable().pipe(takeUntil(this.notifierCompleted), tap((selectNumber) => {
-    console.log(selectNumber);
-    this.arr.filter = (item: any) => {
-      switch (selectNumber) {
-        case 1:
-          return item.price > 1000;
-        case 2:
-          return (item.price < 1000) && (item.price > 500);
-        case 3:
-          return item.price < 100;
-        default:
-          return item;
-      }
-    }
-  }));
-  public products$ = this._http.get<any>('https://dummyjson.com/products').pipe(takeUntil(this.notifierCompleted), tap(({ products }) => {
-    this.arr = new CollectionView(products, {
-      trackChanges: true,
-      canAddNew: true,
-      canCancelEdit: true,
-      pageSize: 4,
-      newItemCreator: () => ({}),
-      //... config here....
-    });
-    this.arr.groupDescriptions.clear();
-    this.arr.groupDescriptions.push(new PropertyGroupDescription('rating', (item) => { // add group
-      if (item.price > 1000) return 'High';
-      if (item.price > 500 && item.age < 1000) return 'Medium';
-      return 'Low';
-    }))
-    this.arr.sortDescriptions.push(new SortDescription('price', false));
-  }));
+  public selectedAction = this.selectedSubject.asObservable();
+  public products$ = this._http.get<any>('https://dummyjson.com/products').pipe(
+    takeUntil(this.notifierCompleted));
   public productWithSelectedAction$ = combineLatest([this.products$, this.selectedAction.pipe(startWith(1)), this.addNewAction$.pipe(startWith(null))]).pipe(takeUntil(this.notifierCompleted),
-    map(([{ products }, selectNumber, itemNew]) => {
-      this.arr.moveToNextPage();
-      return this.arr;
-    }),
-    map(({ groups, ...rest }) => {
-      let productsByGrp = groups.map(gr => {
-        return {
-          name: gr.name,
-          products: gr.items
-        }
-      })
-      return productsByGrp
-    })
+
   )
+
 
   private stopObs() {
     this.notifierCompleted.next(null);
